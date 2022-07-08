a6b8b41edadd49993c43be3e4a0307821e01ad30
flashgrinder <flashgrinder92@gmail.com>
2022-07-08 15:50:52 +0400

    test theme color site

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
-    <meta name="theme-color" content="#000" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="#000" />
+    <!-- <meta name="theme-color" content="#000" /> -->
+    <!-- <meta name="apple-mobile-web-app-status-bar-style" content="#000" /> -->
     <?php wp_head(); ?>
 </head>
 
